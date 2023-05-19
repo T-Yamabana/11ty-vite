b202f7f4e65417d5df7036f4e2424e4f85cfce83
TakuyaYamabana <yamabana@un-t.com>
2023-05-20 07:46:34 +0900

    feat: Sass追加

--- src/scripts/main.ts
+++ src/scripts/main.ts
@@ -1,4 +1,4 @@
-import '../styles/style.css'
+import '../styles/style.scss'
 import typescriptLogo from '../typescript.svg'
 import viteLogo from '/vite.svg'
 import { setupCounter } from './counter.ts'
