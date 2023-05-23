d31e9365fc48676a1b2728f7e8194be36403a130
TakuyaYamabana <yamabana@un-t.com>
2023-05-23 12:25:48 +0900

    feat: Viteサーバーへのプロキシ作成

--- .eleventy.js
+++ .eleventy.js
@@ -1,7 +1,17 @@
+const httpProxy = require("http-proxy");
+
 module.exports = function(eleventyConfig) {
 
   eleventyConfig.setServerOptions({
-    domDiff: false,
+    module: "@11ty/eleventy-server-browsersync",
+    callbacks: {
+      ready: function(_err, browserSync) {
+        const proxy = httpProxy.createProxyServer();
+        browserSync.addMiddleware('*', (req, res) => {
+          proxy.web(req, res, { target: 'http://localhost:5173' });
+        });
+      },
+    },
   });
 
   return {
